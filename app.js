01ae0c52a25f4308f442d9e0d3bac4422c09f4f4
songguoguo927 <1351443467@qq.com>
2019-03-08 17:11:07 +0800

    验证加密逻辑的封装

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 'use strict'
 
 var Koa = require('koa')
-var sha1 = require('sha1')
+var wechat = require('./wexin/g')
+
 var config = {
 	wechat:{
 		appID:'wx68a4e993aa4ac1d5',
-		appSecert:'caa0bf4d5db9836b88479b63b0fbfe93',
+		appSecret:'caa0bf4d5db9836b88479b63b0fbfe93',
 		token:'zidingyitoken'
 	}
 }
 var app = new Koa()
 
-app.use(function *(next){
-	console.log(this.query)
+app.use(wechat(config.wechat))
 
-	var token = config.wechat.token
-	var signature =this.query.signature
-	var nonce = this.query.nonce
-	var timestamp = this.query.timestamp
-	var echostr = this.query.echostr
- 
-	var str = [token, timestamp, nonce].sort().join('')
-	var sha = sha1(str)
+app.listen(80)
 
-	if (sha === signature){
-		this.body = echostr + ''
-	}
-	else{
-		this.body = 'wrong'
-	}
-
-	
-})
-
-app.listen(1234)
-
-console.log('listening:1234')
+console.log('listening:80')
--- wexin/g.js
+++ wexin/g.js
@@ -1,20 +1,12 @@
 'use strict'
 
-var Koa = require('koa')
 var sha1 = require('sha1')
-var config = {
-	wechat:{
-		appID:'wx68a4e993aa4ac1d5',
-		appSecert:'caa0bf4d5db9836b88479b63b0fbfe93',
-		token:'zidingyitoken'
-	}
-}
-var app = new Koa()
 
-app.use(function *(next){
+module.exports = function (opts) {
+    return function *(next){
 	console.log(this.query)
 
-	var token = config.wechat.token
+	var token = opts.token
 	var signature =this.query.signature
 	var nonce = this.query.nonce
 	var timestamp = this.query.timestamp
@@ -22,17 +14,12 @@ app.use(function *(next){
  
 	var str = [token, timestamp, nonce].sort().join('')
 	var sha = sha1(str)
-
+	console.log(sha,signature);
 	if (sha === signature){
 		this.body = echostr + ''
 	}
 	else{
 		this.body = 'wrong'
-	}
-
-	
-})
-
-app.listen(80)
-
-console.log('listening:80')
+	}	
+}
+}
