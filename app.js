a11e7c62cd93df445e923a5f00db8725d90262fd
songguoguo927 <1351443467@qq.com>
2019-03-15 17:29:20 +0800

    获取并保存access_token

--- app.js
+++ app.js
@@ -19,13 +19,49 @@ class API {
     this.appid = appid
     this.appsecret = appsecret
     this.prefix = 'https://api.weixin.qq.com/cgi-bin/'  
-  }
-  async getAccessToken() {
-  
-    const response = await axios.get(`${this.prefix}token?grant_type=client_credential&appid=${this.appid}&secret=${this.appsecret}`)   
-     console.log(response.data)
-  } 
+  // 保存access_token
+    this.saveToken = async function (token) {
+      await fs.writeFile('access_token.txt', JSON.stringify(token))
+    }
+    // 从文件获取读取数据
+    this.getToken = async function () {
+      const txt = await fs.readFile('access_token.txt', 'utf8')
+      return JSON.parse(txt)
+    }
 }
+  // 从https接口获取access_token
+  async getAccessToken() {
+    let token = {}
+    const response = await axios.get(`${this.prefix}token?grant_type=client_credential&appid=${this.appid}&secret=${this.appsecret}`)
+
+    // 过期时间，因网络延迟等，将实际过期时间提前20秒，以防止临界点
+    const expireTime = Date.now() + (response.data.expires_in - 20) * 1000
+    token.accessToken = response.data.access_token
+    token.expireTime = expireTime
+    await this.saveToken(token)
+    return token
+  }
+  
+  // 读取文件获取token，读取失败重新请求接口
+  async ensureAccessToken() {
+    let token = {}
+    try {
+      token = await this.getToken()
+    } catch (e) {
+      token = await this.getAccessToken()
+    }
+    if(token && (this.isValid(token.accessToken, token.expireTime))) {
+      return token
+    }
+    return this.getAccessToken()
+  }
+
+  // 验证access_token是否过期
+  isValid(accessToken, expireTime) {
+    return !!accessToken && Date.now() < expireTime
+  }
+}
+
 const api = new API(config.wechat.appid, config.wechat.appsecret)
 // const api = new API(appid, appsecret)
 api.getAccessToken()
